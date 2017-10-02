4541909a7a6e5bd23bb694aa0c7cc57b44e33153
Bryan Ferris <saffsnail@gmail.com>
2017-10-02 09:47:26 -0700

    Started parallel implementation

---
+++ pi/parallel.c
@@ -0,0 +1,30 @@
+#include "omp"      // The OpenMP framework, which is used for parallelism
+#include <stdlib.h> // rand, RAND_MAX
+#include <stdio.h>  // printf
+#include <time.h>   // time
+
+#define DIAMETER (RADIUS * 2)
+#define RADIUS 1
+
+#define NPOINTS 1000000
+
+int main() {
+  int circle_count = 0;
+  srand(time(NULL));
+# pragma omp parallel num_threads(NPOINTS)
+  {
+    // generate a point inside the square
+    double x = ((rand() / (double)RAND_MAX) * DIAMETER) - RADIUS,
+           y = ((rand() / (double)RAND_MAX) * DIAMETER) - RADIUS;
+
+    if ((x * x) + (y * y) <= (RADIUS * RADIUS))
+    {
+#     pragma omp atomic
+      ++circle_count;
+    }
+  }
+
+  }
+  printf("Pi: %f\n", 4 * ((double)circle_count / (double)NPOINTS));
+}
+
