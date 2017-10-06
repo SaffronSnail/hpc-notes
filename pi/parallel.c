3eefbefe5ab9340bc733e580d5c84e2d566a05d3
Bryan Ferris <saffsnail@gmail.com>
2017-10-05 17:20:30 -0700

    Fixed thread-spawning bug in parallel.c
    As it turns out, spawning 1,000,000 threads is a bad idea

--- pi/parallel.c
+++ pi/parallel.c
@@ -1,4 +1,3 @@
-#include "omp"      // The OpenMP framework, which is used for parallelism
 #include <stdlib.h> // rand, RAND_MAX
 #include <stdio.h>  // printf
 #include <time.h>   // time
@@ -11,7 +10,8 @@
 int main() {
   int circle_count = 0;
   srand(time(NULL));
-# pragma omp parallel num_threads(NPOINTS)
+# pragma omp for nowait
+  for (int i = 0; i < NPOINTS; ++i)
   {
     // generate a point inside the square
     double x = ((rand() / (double)RAND_MAX) * DIAMETER) - RADIUS,
@@ -24,7 +24,6 @@ int main() {
     }
   }
 
-  }
   printf("Pi: %f\n", 4 * ((double)circle_count / (double)NPOINTS));
 }
 
